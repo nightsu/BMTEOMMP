74fdd97fd25f6a3f3962c0acb64a2d739527deba
nightsu <ensucao@qq.com>
2019-05-24 23:36:01 +0800

    photo 模块

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import dva from "dva";
 import "./index.css";
 import 'antd/dist/antd.css';
+import createLoading from 'dva-loading';
 
 // 1. Initialize
 const app = dva();
 
 // 2. Plugins
 // app.use({});
+app.use(createLoading());
 
 // 3. Model
 app.model(require('./models/system').default);
+app.model(require('./models/photo').default);
 
 // 4. Router
 app.router(require("./router").default);
--- src/pages/Photo/index.js
+++ src/pages/Photo/index.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Photo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  }
-  }
-  render() { 
-    return ( 
-      <h1>Photo</h1>
-     );
-  }
-}
+// class Photo extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {  }
+//   }
+//   render() { 
+//     return ( 
+//       <h1>Photo</h1>
+//      );
+//   }
+// }
  
-export default Photo;
+// export default Photo;
+import PhotoCardList from './components/photoCardList';
+
+export default function() {
+  return (
+    <div style={{padding: "24px"}}>
+     <PhotoCardList></PhotoCardList>
+    </div>
+  );
+}
